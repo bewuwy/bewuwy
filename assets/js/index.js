191faad1c8b175a3b4513ebac2af1a9cd30cd5df
bewu <bewuwy@gmail.com>
2022-10-23 17:40:21 +0200

    cleaned up index code

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,51 +5,57 @@ window.addEventListener("scroll", function () {
 
 function showFunction() {
   let root = document.documentElement;
-  
-  let e1 = document.getElementById("sub1");
-  let e2 = document.getElementById("sub2");
-  let arrow = document.getElementById("arrow");
+  let page = document.getElementById("page1");
+
   let title = document.getElementById("title");
+  let sub1 = document.getElementById("sub1");
+  let sub2 = document.getElementById("sub2");
+
   let img = document.getElementById("img");
-  let p = document.getElementById("page1");
-  let socials = document.getElementById("socials");
-  
+  let socials = document.getElementById("iconbox");
+
+  let arrow = document.getElementById("arrow");
+
+  let showInitial = [sub2, arrow];
+  let showAfter = [sub1, socials];
+
   const scrollToTrigger = 120;
   const scrollAmount =
-  document.body.scrollTop || document.documentElement.scrollTop;
-  
-  root.style.setProperty("--scrolled-down", scrollAmount * -0.03 + "em");
-  
-  if (scrollAmount > scrollToTrigger) {
-    // document.getElementById(id1).style.display = "inline";
-    title.classList.add('scale-05');
-    img.classList.add('scale-05');
-    
-    e2.style.display = "none";
-    
-    e1.classList.add("show");
-    e1.classList.remove("hide-display");
-    
-    socials.classList.add("show");
-    socials.classList.remove("hide");
-    
-    arrow.style.visibility = "hidden";
+    document.body.scrollTop || document.documentElement.scrollTop;
 
-    p.classList.remove('narrow');
+  root.style.setProperty("--scrolled-down", scrollAmount * -0.03 + "em");
+
+  if (scrollAmount > scrollToTrigger) {
+    title.classList.add("scale-05");
+    img.classList.add("scale-05");
+    page.classList.remove("narrow");
+
+    showInitial.forEach((element) => {
+      element.classList.remove("show");
+      element.classList.add("hide");
+    });
+
+    showAfter.forEach((element) => {
+      element.classList.add("show");
+      element.classList.remove("hide");
+      element.classList.remove("hide-display");
+    });
   } else {
-    title.classList.remove('scale-05');
-    img.classList.remove('scale-05');
-    
-    e2.style.display = "inline";
-    
-    e1.classList.add("hide-display");
-    e1.classList.remove("show");
-    
-    socials.classList.remove("show");
-    socials.classList.add("hide");
-    
-    arrow.style.visibility = "visible";
-    
-    p.classList.add('narrow')
+    title.classList.remove("scale-05");
+    img.classList.remove("scale-05");
+    page.classList.add("narrow");
+
+    showInitial.forEach((element) => {
+      element.classList.add("show");
+      element.classList.remove("hide");
+      element.classList.remove("hide-display");
+    });
+
+    showAfter.forEach((element) => {
+      element.classList.remove("show");
+      element.classList.add("hide");
+    });
+
+    sub1.classList.add("hide-display");
   }
 }
