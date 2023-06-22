e0875ff683e0043edbe961a55e2d1cc54177ea95
bewu <bewuwy@gmail.com>
2023-06-22 22:51:31 +0200

    initial version

--- app/page.tsx
+++ app/page.tsx
@@ -1,94 +1,40 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import './page.scss'
+import { Fredoka } from 'next/font/google'
+
+const fredoka = Fredoka({ weight: "400", subsets: ["latin"] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className={fredoka.className}>
+
+      <div className="bewu-text">
+        <span>bewu.dev</span>
+      </div>
+
+      <div className="rhs">
+      
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
+        <span className='hi'>Hi, I'm bewu and I'm a dev</span>
+
+        <div className='content'>
+          <div className="projects tile">
+            <span className='title'>My projects</span>
+            <span className='subtitle'>They may not be good, but they are mine</span>
+            <a role="button">See my projects</a>
+            <span className='annotation'>(actually they are good)</span>
+          </div>
+          <div className='me'>
+            <div className='about tile'>
+
+              <span className='title'>About me</span>
+            </div>
+
+            <div className='literally-me tile'>
+              <span className='title'>Literally me</span>
+            </div>
+          </div>
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   )
